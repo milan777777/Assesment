2aa88d39ec047d4f3ed9eeaf38f5eed8f6947171
milan777777 <136436245+milan777777@users.noreply.github.com>
2024-11-18 11:39:51 +0545

    Add files via upload

---
+++ score.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+file_path = '/Users/milangodar/Downloads/Expense.xlsx'
+sheet_name = "sheet2"
+df = pd.read_excel(file_path)
+print("Original Data:")
+print(df)
+sorted_df = df.sort_values(by='Goals', ascending=True)
+
+print("\nSorted Data:")
+print(sorted_df)
+
+sorted_df.to_excel('sorted_excel_file.xlsx', index=False)
+
+print("\nSorted data saved to 'sorted_excel_file.xlsx'")
